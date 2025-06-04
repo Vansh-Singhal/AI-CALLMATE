a6a41ed9a157c5b204f395ab78ceb9526361c106
Vansh Singhal <vansh.singhal@hotmail.com>
2025-06-05 02:36:05 +0530

    AUTH COMPLETED

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+import { toast } from "sonner";
+import { APIResponse } from "@/types/APIResponse";
+import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signInSchema } from "@/schema/signInSchema";
+import { signIn } from "next-auth/react";
+
+const page = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+    const result = await signIn("credentials", {
+      redirect: false,
+      email: data.email,
+      password: data.password,
+    });
+    setIsSubmitting(false);
+
+    if (result?.error) {
+      toast.error("Login Failed");
+    }
+
+    if (result?.url) {
+      toast.success("Login Successful");
+      router.replace("/dashboard");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Create your smart meeting
+          </h1>
+          <p className="mb-4">Sign in to your account</p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="john.doe@gmail.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              disabled={isSubmitting}
+              className="w-full cursor-pointer"
+            >
+              {isSubmitting ? (
+                <Loader2 className="size-4 animate-spin" />
+              ) : (
+                "Sign in"
+              )}
+            </Button>
+          </form>
+        </Form>
+
+        <div className="text-center">
+          <p>
+            Not a member?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/schema/signInSchema.ts
+++ src/schema/signInSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const signInSchema = z.object({
-  identifier: z.string().email(),
+  email: z.string().email(),
   password: z.string(),
 });
